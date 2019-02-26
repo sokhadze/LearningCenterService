af71240112b254a41eb7982d1e5296ebe6560b88
sokhadze <lerisokhadze@gmail.com>
2019-02-26 21:15:56 +0400

    update

---
+++ ServiceApp/DataContracts/SubjectDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServiceApp
+{
+    [DataContract]
+    public class SubjectDTO
+    {
+        public int ID { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Code { get; set; }
+        [DataMember]
+        public int Credits { get; set; }
+        [DataMember]
+        public int? Hours { get; set; }
+    }
+}
---
+++ ServiceApp/ServiceContracts/ISubjectsManagement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServiceApp
+{
+    [ServiceContract]
+    public interface ISubjectsManagement
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate= "/GetAllSubjects")]
+        List<SubjectDTO> GetAllSubjects();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/GetSubjectByCode/{code}")]
+        List<SubjectDTO> GetSubjectByCode(string code);
+    }
+
+}
